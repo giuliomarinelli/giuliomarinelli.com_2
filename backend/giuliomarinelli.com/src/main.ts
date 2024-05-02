8d47b6a3d218cb381fcba61c509e630c5caa4aa4
giuliomarinelli <giulio.marinelli@icloud.com>
2024-05-02 20:39:07 +0200

    implemented configuration of app

--- backend/giuliomarinelli.com/src/main.ts
+++ backend/giuliomarinelli.com/src/main.ts
@@ -2,19 +2,27 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
+import fastifyCookie from '@fastify/cookie';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter()
   );
+  const logger = new Logger('Bootstrap')
   const configSvc = app.get<ConfigService>(ConfigService)
+  await app.register(fastifyCookie, {
+    secret: configSvc.get<string>('KEYS.cookieSignSecret')
+  });
+  const port = configSvc.get<number>('APP.port')
   app.enableCors({
     origin: configSvc.get<string[]>('APP.CorsOrigins'),
     credentials: true,
     methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE'],
     allowedHeaders: '*'
   })
-  await app.listen(3000);
+  await app.listen(port);
+  logger.log('Fastify Web Server listening on port ' + port)
 }
 bootstrap();
