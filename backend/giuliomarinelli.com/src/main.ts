ec929096aab7498e8092e979664efb8c5473aa51
giuliomarinelli <giulio.marinelli@icloud.com>
2024-05-02 20:25:54 +0200

    new fastify monolythic app

--- backend/giuliomarinelli.com/src/main.ts
+++ backend/giuliomarinelli.com/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
+  const configSvc = app.get<ConfigService>(ConfigService)
+  app.enableCors({
+    origin: configSvc.get<string[]>('APP.CorsOrigins'),
+    credentials: true,
+    methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    allowedHeaders: '*'
+  })
   await app.listen(3000);
 }
 bootstrap();
