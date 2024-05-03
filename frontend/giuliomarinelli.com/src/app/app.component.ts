0bd0d42c530d2150da5748140fe347b506da238c
giuliomarinelli <giulio.marinelli@icloud.com>
2024-05-04 01:08:20 +0200

    styling layout

--- frontend/giuliomarinelli.com/src/app/app.component.ts
+++ frontend/giuliomarinelli.com/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, afterNextRender } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
-  selector: 'app-root',
+  selector: '#root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  constructor() {
+    afterNextRender(() => {
+      if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        console.log('dark mode')
+      } else {
+        console.log('light mode')
+      }
+    })
+  }
+
+  @ViewChild('sidenav') protected sidenav!: MatSidenav;
+
+
+
+
   title = 'giuliomarinelli.com';
 }
--- frontend/giuliomarinelli.com/src/app/app.module.ts
+++ frontend/giuliomarinelli.com/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,9 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSidenavModule,
+    MatToolbarModule
   ],
   providers: [
     provideClientHydration(),
