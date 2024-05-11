23ef80533c83ae5fca08ee59fbab24b7b83aea6b
giuliomarinelli <giulio.marinelli@icloud.com>
2024-05-11 21:44:18 +0200

    created AuthUserModule for authentrication

--- backend/giuliomarinelli.com/src/app.module.ts
+++ backend/giuliomarinelli.com/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { configurations } from './config/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthUserModule } from './nest_modules/auth-user/auth-user.module';
 
 @Module({
   imports: [
@@ -14,7 +15,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       imports: [ConfigModule],
       useFactory: (configSvc: ConfigService) => configSvc.get('DB'),
       inject: [ConfigService]
-    })
+    }),
+    AuthUserModule
   ],
   controllers: [],
   providers: [],
